c8295a9e90191c7136150241457cf02d5e3db596
gultar <sacha-olivier.dulac@hotmail.com>
2018-06-22 13:25:46 -0400

    encrypted api key

--- scripts/iching.js
+++ scripts/iching.js
@@ -170,21 +170,24 @@ class Hexagram{
 //Configures the firebase DB and fetches the corresponding hexagram text
 function fetchHexFromFireBase(hexNumber){
     var appName = ('app-'+hexNumber).toString();
-    var config = {
-    apiKey: "AIzaSyDvHcao89Mne4IttxjNop6phnaqTo-jxcA",
-    authDomain: "iching-1f68d.firebaseapp.com",
-    databaseURL: "https://iching-1f68d.firebaseio.com",
-    projectId: "iching-1f68d",
-    storageBucket: "iching-1f68d.appspot.com",
-    messagingSenderId: "711518649160"
-  };
+    var config = '54737be9255054376f9fb731b344f245e917ec5ea8a72d9f63d5671378dd915bdc462cd709515cbe'+
+'a6da84d32eb9d8e611353f377bdf0e16b1d2ec0fa0e5aa684c419d66a836a99851e2649c578afb66'+
+'aad7fef9bcea500ca063884448a575ad9bafcad00bcb93ec5ee152af6669352605bca8b216207aff'+
+'22fed3432757b96dc0621e6063e7d10ea365ff0e28b00da5ffe5e60163586967e786efe2134bc5c5'+
+'c8a58b136950875eff84f1422dfb31f929b7c876789924792ddcc9bd4bc9acbbb6bc11a09a0c1e05'+
+'5a9a7a8e03c22e77ee5e5556fb2a150879882758f86e6a050e443a073730d99508ced3c9f98fd330'+
+'de210477be7e6e4f24a7514aa4'
+    /*
+      Had to remode google api key
+    	Check terminal_app for it
+    */
+
 
   if(firstTime){
     appName = '[DEFAULT]';
     firstTime = false;
   }
   firebase.initializeApp(config, appName);
-
 
     var hexNumberIndex = hexNumber - 1;
 
